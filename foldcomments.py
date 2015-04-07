b8cb2384bac83c2af1003e48e979f1275a62e0eb
Oskar Olsson <oskar.ols@gmail.com>
2015-04-07 19:32:31 +0200

    Improved code legibility and bugfixes
    - Improved checking whether comments are adjacent or not.
      Now stripping all whitespace between comments instead
      of just indentations (thanks @junstyle)
    - Instead of using a Mixin comments now have their own Class.
    - Misc restructuring and comments

--- foldcomments.py
+++ foldcomments.py
@@ -1,186 +1,189 @@
 from sublime import Region, Settings, load_settings
 import sublime_plugin
 
+from itertools import tee, chain
+
+try:
+    from itertools import izip as zip
+except ImportError: # will be 3.x series
+    pass
+
 settings = load_settings("foldcomments.sublime-settings")
 
 
-def is_multi_line(view, region):
+def previous_and_current(iterable, *iterables):
+    """
+    Includes the previous value of iterable in iteration
+
+    > previous_and_current([1,2,3])
+    => (None, 1)
+    => (1, 2)
+    => (2, 3)
+    """
+    prevs, items = tee(iterable, 2)
+    # Offset for the first element, since has no previous value
+    prevs = chain([None], prevs)
+    return zip(prevs, items, *iterables)
+
+def is_comment_multi_line(view, region):
     return len(view.lines(region)) > 1
 
+def normalize_comment(view, region):
+    if is_comment_multi_line(view, region):
+        return normalize_multiline_comment(view, region)
+    else:
+        return normalize_singleline_comment(view, region)
 
-class CommentsMixin:
 
-    def post_process_comments(self, comments):
-        """ Apply settings """
-        view = self.view
+def normalize_singleline_comment(view, region):
+    """
+    Since single line comments include the newline
+    if we don't explicitly make sure newline is kept
+    out of the fold indicator, it will munge together
+    with code. Example:
+
+    // This is an example comment
+    function foo() {
+
+    Becomes:
+
+    (..) function foo() {
+
+    When what we really want is to keep the fold
+    on it's own line, like so:
+
+    (..)
+    function foo() {
+    """
+    region_str = view.substr(region)
+    last_newline = region_str.rfind('\n')
+
+    if (last_newline == -1):
+        # Single-line block comments don't include
+        # their newline.
+        # /* foo bar baz */ <-- like this
+        return region
+    else:
+        return Region(region.begin(), region.begin() + last_newline)
+
+
+def normalize_multiline_comment(view, region):
+    """
+    This is needed since in some languages it seems
+    the boundaries for proper block-comments
+    and chained single-line comments differ. The
+    chaines single-line comments have the last point
+    ( .end() .b etc) of their region set to the subsequent line,
+    while the block comments have it set to the last char
+    of their last line.
+
+    Example where the @ char signifies
+    the last endpoint:
+
+    BLOCK COMMENT
+
+    /**
+     * This is an example comment
+     */@ <---
+    function foobar() {
+
+    MULTIPLE SINGLE COMMENTS
+
+    //
+    // This is an example comment
+    //
+    @function foobar() { <---
+
+    What we do to fix this is not to use the boundaries
+    for the regions, but instead use the last line
+    for the region - which seems to have the correct end
+    point set.
+    """
+    lines = view.lines(region)
+    last_line = lines[-1]
+    last_point = last_line.b
+    return Region(region.a, last_point)
+
+
+class CommentNodes:
+
+    def __init__(self, view):
+        self.comments = None # collection of Region objects
+        self.view = view
+        self.find_comments()
+        self.apply_settings()
+
+    def find_comments(self):
+        self.comments = [
+            normalize_comment(self.view, c) for c in self.view.find_by_selector('comment')
+        ]
+
+    def apply_settings(self):
+        if not settings.get('fold_single_line_comments'):
+            self.remove_single_line_comments()
 
         if settings.get('concatenate_adjacent_comments'):
-            comments = self.concatenate_adjacent_comments(comments)
+            self.concatenate_adjacent_comments()
 
-        if not settings.get('fold_single_line_comments'):
-            comments = self.remove_single_line_comments(comments)
+    def remove_single_line_comments(self):
+        self.comments = [c for c in self.comments if is_comment_multi_line(self.view, c)]
 
-        return comments
-
-    def remove_single_line_comments(self, comments):
-        return [c for c in comments if is_multi_line(self.view, c)]
-
-    def concatenate_adjacent_comments(self, comments):
+    def concatenate_adjacent_comments(self):
         """
-        Do rudimentary stripping of comment indentation and
-        appendage and then concatenate if one begins
-        where another one has ended
+        Merges any comments that are adjacent.
         """
-        view = self.view
 
-        # view.line(region) strips indentations etc
-        stripped_comments = list(map(view.line, comments))
-        conc_comments = []
+        def concatenate(region1, region2):
+            return region1.cover(region2)
 
-        for index, stripped_comment in enumerate(stripped_comments):
-            previous_stripped_comment = stripped_comments[index - 1]
+        def is_adjacent(region1, region2):
+            region_inbetween = Region(region1.end(), region2.begin())
+            return len(self.view.substr(region_inbetween).strip()) == 0
 
-            if not previous_stripped_comment:  # first item
-                conc_comments.append(comments[index])
-                continue
+        concatenated_comments = []
 
-            if previous_stripped_comment.end() + 1 == stripped_comment.begin():
-                conc_comments[-1] = conc_comments[-1].cover(comments[index])
-            else:
-                conc_comments.append(comments[index])
+        for prev_comment, comment in previous_and_current(self.comments):
+            concatenated_comment = None
 
-        return conc_comments
+            # prev wont be set on first iteration
+            if prev_comment and is_adjacent(prev_comment, comment):
+                concatenated_comment = concatenate(concatenated_comments.pop(), comment)
 
-    def comment_regions(self):
-        view = self.view
+            concatenated_comments.append(concatenated_comment or comment)
 
-        def normalize_multiline_comment(region):
-            """
-            This is needed since in some languages it seems
-            the boundaries for proper block-comments
-            and chained single-line comments differ. The
-            chaines single-line comments have the last point 
-            ( .end() .b etc) of their region set to the subsequent line, 
-            while the block comments have it set to the last char
-            of their last line. 
+        self.comments = concatenated_comments
 
-            Example where the @ char signifies
-            the last endpoint:
+    def fold(self):
+        self.view.fold(self.comments)
 
-            BLOCK COMMENT
+    def unfold(self):
+        self.view.unfold(self.comments)
 
-            /**
-             * This is an example comment
-             */@ <--- 
-            function foobar() {
+    def toggle_folding(self):
+        def is_folded(comments):
+            return self.view.unfold(comments[0])  # False if /already folded/
 
-            MULTIPLE SINGLE COMMENTS
-
-            // 
-            // This is an example comment
-            //
-            @function foobar() { <---
-
-            What we do to fix this is not to use the boundaries
-            for the regions, but instead use the last line
-            for the region - which seems to have the correct end
-            point set.
-            """
-            lines = view.lines(region)
-            last_line = lines[-1]
-            last_point = last_line.b
-            return Region(region.a, last_point)
-
-        def normalize_singleline_comment(region):
-            """
-            Since single line comments include the newline
-            if we don't explicitly make sure newline is kept
-            out of the fold indicator, it will munge together
-            with code. Example:
-
-            // This is an example comment
-            function foo() {
-
-            Becomes:
-
-            (..) function foo() {
-
-            When what we really want is to keep the fold
-            on it's own line, like so:
-
-            (..)
-            function foo() {
-            """
-            region_str = view.substr(region)
-            last_newline = region_str.rfind('\n')
-
-            if (last_newline == -1):
-                # Single-line block comments don't include
-                # their newline.
-                # /* foo bar baz */ <-- like this
-                return region
-            else:
-                return Region(region.begin(), region.begin() + last_newline)
-
-        def normalize_regions(region):
-            if is_multi_line(view, region):
-                return normalize_multiline_comment(region)
-            else:
-                return normalize_singleline_comment(region)
-
-        return [normalize_regions(c) for c in view.find_by_selector('comment')]
+        self.unfold() if is_folded(self.comments) else self.fold()
 
 
 # ================================= COMMANDS ==================================
 
-
-class ToggleFoldCommentsCommand(sublime_plugin.TextCommand, CommentsMixin):
-
-    def run(self, edit):
-        view = self.view
-        comments = self.comment_regions()
-
-        def is_folded(comments):
-            return view.unfold(comments[0]) # False if /already folded/
-
-        if not comments:
-            return
-
-        comments = self.post_process_comments(comments)
-
-        if not comments:
-            return
-
-        if not is_folded(comments):
-            view.fold(comments)
-        else:
-            view.unfold(comments)
-
-
-class FoldCommentsCommand(sublime_plugin.TextCommand, CommentsMixin):
+class ToggleFoldCommentsCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        view = self.view
-        comments = self.comment_regions()
-
-        if not comments:
-            return
-
-        comments = self.post_process_comments(comments)
-
-        if not comments:
-            return
-        
-        view.fold(comments)
+        comments = CommentNodes(self.view)
+        comments.toggle_folding()
 
 
-class UnfoldCommentsCommand(sublime_plugin.TextCommand, CommentsMixin):
+class FoldCommentsCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        view = self.view
-        comments = self.comment_regions()
+        comments = CommentNodes(self.view)
+        comments.fold()
 
-        if not comments:
-            return
 
-        view.unfold(comments)
+class UnfoldCommentsCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        comments = CommentNodes(self.view)
+        comments.unfold()
